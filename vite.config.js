bd66d7baf4388d543a512c7c706bcb6ca7f30b7b
raynehcuba-dotcom <raynehcuba@gmail.com>
2026-09-17 11:54:38 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/gestor_inventario/', // ← ESTA ES LA LÍNEA CLAVE
+  base: '/gestor_inventario/', // ← Esta es la única línea que importa agregar
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
